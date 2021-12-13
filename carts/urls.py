b2c151500c3d18c75b4e396696ab670a87b66014
John Doe <Johndoe@example.com>
2021-12-13 21:19:35 +0100

    handled cart

--- carts/urls.py
+++ carts/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('remove_cart/<int:product_id>/<int:cart_item_id>/', views.remove_cart, name='remove_cart'), ## slash bardzo ważny
     path('remove_cart_item/<int:product_id>/<int:cart_item_id>/', views.remove_cart_item, name='remove_cart_item'), ## slash bardzo ważny
 
+    path('checkout/', views.checkout, name='checkout'),
+
 ]
