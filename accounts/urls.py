bff5fa335a6a19a4014b607440532bef1b25033f
John Doe <Johndoe@example.com>
2021-12-27 17:42:10 +0100

    product order and payments

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
     path('resetPassword/', views.resetPassword, name='resetPassword'),
     path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
-
+    
 ]
